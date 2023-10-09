37ebb5903b3749e8d3968ffca3cd29ad11e7d7aa
Abdul Ahad Khan <ahadaak231@gmail.com>
2023-10-09 10:15:38 +0400

    about page styling completed

--- components/about.tsx
+++ components/about.tsx
@@ -4,10 +4,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section>
-        <h2>About me</h2>
+    <section className="mb-28 max-w-[52rem] text-center leading-8 sm:mb-40">
+        <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+        <p className="mb-3 ">
+            Ever since a young age, I have been fascinated with everything tech. It started with messing about with computers and now to building full fledged apps. I graduated in June 2021 with a degree in <span className="font-medium">Computer Engineering</span> and developed a strong interest in providing solutions to problems. Throughout my University and career, I have built projects that involve <span className="italic">Deep learning, SaaS, AWS, Restful APIs, Springboot, React</span> and much <span className="font-medium">more</span>.
+        </p>
         <p className="mb-3">
-            Ever since a young age I have been fascinated with everything tech. It started with messing about with computers and now to building full fledged apps. I graduated in June 2021 with a degree in Computer Engineering and developed a strong interest in providing solutions to problems. Throughout my University and career I have built projects which involve deep learning, SaaS, AWS, Restful APIs, Springboot, React and much more. I am currently looking for full-time junior opportunities in Software Development, Business Analytics or Product Management. When I am not working or building projects, I like to play and watch football (Real Madrid fan btw). I am also an active gamer of AAA titles and I also occasionally play guitar as well. 
+            I am currently looking for full-time <span className="font-medium">Junior</span> opportunities in <span className="font-medium">Software Development</span> or <span className="font-medium">Product Management</span>. 
+        </p>
+        <p>
+            When I am <span className="italic">not working</span> or building projects, I like to play and watch football (Real Madrid fan btw). I am also an active gamer of AAA titles and I also occasionally play guitar as well. 
         </p>
     </section>
   )
