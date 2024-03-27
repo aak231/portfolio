dcbf55aa5da796584c98bdba5da98c98ff0711b0
Abdul Ahad Khan <ahadaak231@gmail.com>
2024-03-28 02:31:04 +0400

    fixed the even bit

--- components/project.tsx
+++ components/project.tsx
@@ -28,9 +28,9 @@ export default function Project({
         opacity: opacityProgress,
       }}
       ref={ref}
-      className="mb-3 sm:mb-8 last:mb-0"
+      className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[28rem]  even:pl-8 hover:bg-gray-200 transition">
+      <section className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[28rem] rounded-lg group-even:pl-8 hover:bg-gray-200 transition">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 mb-6">
