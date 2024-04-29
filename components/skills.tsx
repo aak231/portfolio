65070da320c8d053c362bf61bbd2e536de61136d
Abdul Ahad Khan <ahadaak231@gmail.com>
2024-04-29 14:28:52 +0400

    added skills component

---
+++ components/skills.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionHeader from './sectionHeader'
+
+export default function Skills() {
+  return (
+    <section>
+        <SectionHeader>Skills</SectionHeader>
+    </section>
+  )
+}
